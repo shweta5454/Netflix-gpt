acb7023f5a066282f21f8a899923e4d1fcca820e
shweta5454 <shwetasonth98@gmail.com>
2023-09-08 23:30:42 +0530

    GPT Seach Page

--- src/components/GptsearchBar.js
+++ src/components/GptsearchBar.js
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useRef } from "react";
 import { lang } from "../utils/languageConstants";
 import { useSelector } from "react-redux";
+import openai from "../utils/openai";
+import { API_OPTIONS } from "../utils/constants";
 
 const GptsearchBar = () => {
   const langKey = useSelector((store) => store.appconfig.lang);
-  // console.log(lang);
+  const searchText = useRef(null);
+  //search movie in TMDB
+  const searchMovieTMDB = async (moviename) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        moviename +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+  //gpt search function
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value);
+    //Make an API call to GPT API and get movie results
+    // const gptQuery =
+    //   "Act as a Movie Recommendation system and suggest some movies for the query" +
+    //   searchText.current.value +
+    //   ". only give me names of 5 movies,comma separated leke the example result given ahead.Example Result:Gadar,sholey,Don,golmaal,koi mill gaya";
+    // const getResults = await openai.chat.completions.create({
+    //   messages: [{ role: "user", content: gptQuery }],
+    //   model: "gpt-3.5-turbo",
+    // });
+
+    // console.log(getResults.choices);
+    const getMovies = [
+      "Andaaz apna apna",
+      "hera-pheri",
+      "golmala",
+      "Padosan",
+      "chupke chupke",
+    ];
+
+    //For each movie i will search TMDB API call
+    const promiseArray = getMovies.map((movie) => searchMovieTMDB(movie));
+    //[Promise,Promise,Promise,Promise,Promise]===>to resolve this
+
+    const tmdbResults = await Promise.all(promiseArray);
+    console.log(tmdbResults);
+  };
   return (
     <div className="pt-[10%] ">
-      <form className="w-1/2 mx-auto bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 mx-auto bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearcgPlaceholder}
         />
-        <button className="py-2 px-4 col-span-3 m-4 bg-red-700 text-white rounded-lg">
+        <button
+          className="py-2 px-4 col-span-3 m-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,25 +1,38 @@
-export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const BACKGROUND_IMG="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+export const BACKGROUND_IMG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-export const PHOTOURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmNf7PSvRpbySf-O3h54cG9eKtMNLNBV3G4WFWiTavPw&s"
+export const PHOTOURL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmNf7PSvRpbySf-O3h54cG9eKtMNLNBV3G4WFWiTavPw&s";
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGNlMDQ3NTgwZjAxNTY4Y2JlYTk5ZmViN2UwMDJiYSIsInN1YiI6IjY0ZjVjZjMxNWYyYjhkMDEzOWNmMzFhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jgZ-MMmU1QOGKkddqiH21h0WIT-nRHynmYhGTOj7XSo'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGNlMDQ3NTgwZjAxNTY4Y2JlYTk5ZmViN2UwMDJiYSIsInN1YiI6IjY0ZjVjZjMxNWYyYjhkMDEzOWNmMzFhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jgZ-MMmU1QOGKkddqiH21h0WIT-nRHynmYhGTOj7XSo",
+  },
 };
 
 export const TRAILOR_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGNlMDQ3NTgwZjAxNTY4Y2JlYTk5ZmViN2UwMDJiYSIsInN1YiI6IjY0ZjVjZjMxNWYyYjhkMDEzOWNmMzFhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jgZ-MMmU1QOGKkddqiH21h0WIT-nRHynmYhGTOj7XSo'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGNlMDQ3NTgwZjAxNTY4Y2JlYTk5ZmViN2UwMDJiYSIsInN1YiI6IjY0ZjVjZjMxNWYyYjhkMDEzOWNmMzFhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jgZ-MMmU1QOGKkddqiH21h0WIT-nRHynmYhGTOj7XSo",
+  },
 };
 
-export const IMG_CDN="https://image.tmdb.org/t/p/w500/"
+export const IMG_CDN = "https://image.tmdb.org/t/p/w500/";
 
-export const SUPPORTED_LANGUAGES=[{identifier:"en",name:"English"},{identifier:"hindi",name:"Hindi"},{identifier:"spanish",name:"Spanish"},{identifier:"french",name:"French"}]
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+  { identifier: "french", name: "French" },
+];
+
+export const OPEN_AI_KEY =
+  "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";
